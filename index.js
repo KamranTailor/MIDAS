aa5bb75352743105cfd0d3290b520cbb002f6652
Kamran Tailor <kamrantailor@gmail.com>
2024-11-01 15:22:27 +0000

    Body Passer Updated (2)

--- index.js
+++ index.js
@@ -3,26 +3,30 @@
 const version = "V1";
 
 import express from 'express';
-import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import localUtils from './utils/main.js';
-import fs from 'fs/promises';
 import { v4 as uuidv4 } from 'uuid';
 import nodemailer from 'nodemailer';
 
 dotenv.config();
-const port =  9090;
+const port = 9090;
+
 import app from './main.js';
 
-import { initializePeriodicTasks } from './dump/main.js'; 
+// Initialize periodic tasks
+import { initializePeriodicTasks } from './dump/main.js';
 initializePeriodicTasks();
 
-let controller = await localUtils.setController();
+// Initialize controller asynchronously
+(async () => {
+    let controller = await localUtils.setController();
+})();
 
-app.use(express.json({ limit: '10mb' })); // for JSON payloads
-app.use(express.urlencoded({ limit: '10mb', extended: true })); // for URL-encoded payloads
-
-app.get('/version', (request, response) => {response.json({version: version})});
-app.use(bodyParser.urlencoded({ extended: true }));
+// Set version endpoint and static files
+app.get('/version', (request, response) => {
+    response.json({ version: version });
+});
 app.use(express.static('public'));
+
+// Start server
 app.listen(port, () => console.log(`Listening at port ${port}`));
--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-//main.js
+// main.js
 
 import express from 'express';
-import bodyParser from 'body-parser';
 import localUtils from './utils/main.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
-//Database Commands 
+// Set JSON payload limits and parsing
+app.use(express.json({ limit: '20mb' })); // Adjusted limit to 20MB
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
+
+// Database Commands
 import addEntryRouter from './api/databaseEdit/addEntry.js';
 app.use('/addEntry', localUtils.apiKeyMiddleware, addEntryRouter);
 
@@ -24,7 +27,7 @@ app.use('/getDatabase', localUtils.apiKeyMiddleware, getDatabaseRouter);
 import dataCachingRouter from './api/dataCaching/setData.js';
 app.use('/dataCaching/setData', localUtils.apiKeyMiddleware, dataCachingRouter);
 
-//Web Commands 
+// Web Commands 
 app.use('/webAdminView/edit/*', localUtils.checkAccessToken);
 
 import webRequestToken from "./api/webEditor/login/requestToken.js";
@@ -38,6 +41,5 @@ app.use('/webAdminView/edit/addDatabase', addDatabaseRouter);
 
 import getAllDatabaseRouter from "./api/webEditor/edit/getAllDatabase.js";
 app.use('/webAdminView/edit/getAllDatabase', getAllDatabaseRouter);
-
 
 export default app;
